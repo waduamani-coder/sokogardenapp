ad6b57c313e76167dda89a6479369a62e139e9be
waduamani-coder <waduamani@gmail.com>
2026-03-11 14:05:26 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Addproducts from './components/Addproducts';
 import Signup from './components/Signup';
 import Signin from './components/Signin';
 import Notfound from './components/Notfound';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
+  // initialize the hooks
+  const [username,setusername] = useState("");
+  const [email,setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const[phone,setPhone] = useState("");
   return (
-    <div>
-      <h1>Welcome to the Signup page</h1>
+    <div className="row justify-content-center mt-4">
+      <div className="card col-md-6 shadow p-4"> <h1 className="text-primary">Sign Up</h1> 
+
+      <form>
+        <input type="text"
+        placeholder="Enter the Username" 
+        className='form-control'
+        value={username}
+        onChange={(e) => setusername(e.target.value)} 
+        required/> <br />
+
+
+        {/* {username} */}
+
+        <input type="email"
+        placeholder="Enter the email Address"
+        className="form-control" 
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required/> <br />
+        {/* {email} */}
+
+        <input type="password"
+        placeholder='Enter the Password'
+        className='form-control'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+         /> <br />
+
+        {/* {password} */}
+
+         <input type="tel"
+         placeholder="Enter the Mobile Phone number"
+         className='form-control' 
+         value={phone}
+         onChange={(e) => setPhone(e.target.value)}
+         required/> <br />
+
+         {/* {phone} */}
+
+         <input type="button" value="signup" className="btn btn-primary" /> <br /> <br />
+         Already have an account? <Link to={'/signin'}>Signin</Link>
+
+      </form>
+</div>
+        
     </div>
   )
 }
 
 export default Signup;
+
+//Research on Axios module in reactjs
