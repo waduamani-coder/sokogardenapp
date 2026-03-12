f0c1fe93b27a26848435e136706dff1ebfd2065a
waduamani-coder <waduamani@gmail.com>
2026-03-12 14:05:11 +0300

    commit

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,22 +1,76 @@
+import axios from 'axios';
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 
 const Signup = () => {
   // initialize the hooks
-  const [username,setusername] = useState("");
+  const [username,setUsername] = useState("");
   const [email,setEmail] = useState("");
   const [password, setPassword] = useState("");
   const[phone,setPhone] = useState("");
+
+  // Define the three states and the application will move to
+  const[loading, setLoading] = useState("");
+  const[success, setSuccess] = useState("");
+  const[error, setError] = useState("");
+
+  // Below is a function that will handle the submit action
+  const handleSubmit = async(e) =>{
+    // Below we prevent our site from reloading
+    e.preventDefault()
+
+    // Update our loading hook with a message that will be displayed to the users who are trying to register
+    setLoading("Please wait as registration is in progress...")
+
+    try{
+      // create a form data object that will enable you to capture the four details entered on the form
+      const formdata = new FormData();
+
+      // insert the four details (username,email,password,phone) in terms of key - value pairs
+      formdata.append("username", username);
+      formdata.append("email",email);
+      formdata.append("password",password);
+      formdata.append("phone",phone);
+
+      // by use of axios, we can access be the method post
+      const response =await axios.post("https://waduamani.alwaysdata.net/api/signup", formdata)
+
+      // set back the loading to default
+      setLoading("");
+
+      // just incase everything goes well, update the success hook with a message
+      setSuccess(response.data.message)
+
+      // clear your hooks
+      setUsername("");
+      setEmail("");
+      setPassword("");
+      setPhone("");
+
+    }
+    catch(error){
+      // set the loading back to default
+      setLoading("");
+
+      // update the error hook with the message given back from the response
+      setError(error.message)
+
+    }
+
+  }
   return (
     <div className="row justify-content-center mt-4">
       <div className="card col-md-6 shadow p-4"> <h1 className="text-primary">Sign Up</h1> 
+      <h5 className="text-warning">{loading}</h5>
+      <h3 className="text-success">{success}</h3>
+      <h4 className="text-warning">{error}</h4>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="text"
         placeholder="Enter the Username" 
         className='form-control'
         value={username}
-        onChange={(e) => setusername(e.target.value)} 
+        onChange={(e) => setUsername(e.target.value)} 
         required/> <br />
 
 
@@ -49,7 +103,7 @@ const Signup = () => {
 
          {/* {phone} */}
 
-         <input type="button" value="signup" className="btn btn-primary" /> <br /> <br />
+         <input type="submit" value="signup" className="btn btn-primary" /> <br /> <br />
          Already have an account? <Link to={'/signin'}>Signin</Link>
 
       </form>
